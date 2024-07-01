4dbd3d3b032c4b06ab5a5464596f9292ff1e675b
DixonKH <khaydarovdilshod9@gmail.com>
2024-07-02 02:10:55 +0900

    feat: create Schema model and property enums

---
+++ apps/rent-housing-api/src/libs/enums/property.enum.ts
@@ -0,0 +1,74 @@
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum PropertyType {
+	APARTMENT = 'APARTMENT',
+	DORMITORY = 'DORMITORY',
+	SINGLR_ROOM = 'SINGLR_ROOM',
+	STUDIO = 'STUDIO',
+	HOUSE = 'HOUSE',
+}
+registerEnumType(PropertyType, {
+	name: 'PropertyType',
+});
+
+export enum PropertyAmenity {
+	ELEVATOR = 'ELEVATOR',
+	INTERNET = 'INTERNET',
+	PARKING = 'PARKING',
+	WASHING_MACHINE = 'WASHING_MACHINE',
+	DISHWASHER = 'DISHWASHER',
+	AIR_CONDITIONER = 'AIR_CONDITIONER',
+	VACUUM_CLEANER = 'VACUUM_CLEANER',
+	HEAT_PUMP = 'HEAT_PUMP',
+	FURNITURE = 'FURNITURE',
+	TV = 'TV',
+}
+
+registerEnumType(PropertyAmenity, {
+	name: 'PropertyAmenity',
+});
+
+export enum StayDuration {
+	MONTHLY = 'MONTHLY',
+	HALF_YEATR = 'HALF_YEATR',
+	ANNUAL = 'ANNUAL',
+}
+
+registerEnumType(StayDuration, {
+	name: 'leaseDuration',
+});
+
+export enum PropertyStatus {
+	ACTIVE = 'ACTIVE',
+	SOLD = 'SOLD',
+	DELETE = 'DELETE',
+}
+registerEnumType(PropertyStatus, {
+	name: 'PropertyStatus',
+});
+
+export enum PropertyLocation {
+	SEOUL = 'SEOUL',
+	BUSAN = 'BUSAN',
+	INCHEON = 'INCHEON',
+	DAEGU = 'DAEGU',
+	GYEONGJU = 'GYEONGJU',
+	GWANGJU = 'GWANGJU',
+	CHONJU = 'CHONJU',
+	DAEJON = 'DAEJON',
+	JEJU = 'JEJU',
+	ULSAN = 'ULSAN',
+	GWANGYONG = 'GWANGYONG',
+	GIMHAE = 'GIMHAE',
+	YONGIN = 'YONGIN',
+}
+registerEnumType(PropertyLocation, {
+	name: 'PropertyLocation',
+});
+
+export enum PropertyUtilityBills {
+	GAS = 'GAS',
+	WATER = 'WATER',
+	ELECTRICITY = 'ELECTRICITY',
+	INTERNET = 'INTERNET',
+}
---
+++ apps/rent-housing-api/src/schemas/Property.model.ts
@@ -0,0 +1,141 @@
+import { Schema } from 'mongoose';
+import {
+	PropertyAmenity,
+	PropertyLocation,
+	PropertyStatus,
+	PropertyType,
+	PropertyUtilityBills,
+	StayDuration,
+} from '../libs/enums/property.enum';
+
+const PropertySchema = new Schema(
+	{
+		propertyType: {
+			type: String,
+			enum: PropertyType,
+			required: true,
+		},
+
+		propertyAmenities: {
+			type: String,
+			enum: PropertyAmenity,
+			required: true,
+		},
+
+		stayDuration: {
+			type: String,
+			enum: StayDuration,
+			required: true,
+		},
+
+		propertyStatus: {
+			type: String,
+			enum: PropertyStatus,
+			default: PropertyStatus.ACTIVE,
+		},
+
+		propertyLocation: {
+			type: String,
+			enum: PropertyLocation,
+			required: true,
+		},
+
+		propertyAddress: {
+			type: String,
+			required: true,
+		},
+
+		propertyTitle: {
+			type: String,
+			required: true,
+		},
+
+		propertyPrice: {
+			type: Number,
+			required: true,
+		},
+
+		propertyDeposite: {
+			type: Number,
+			required: true,
+		},
+
+		propertyUtilityBills: {
+			type: String,
+			enum: PropertyUtilityBills,
+			required: true,
+		},
+
+		propertySquare: {
+			type: Number,
+			required: true,
+		},
+
+		propertyBeds: {
+			type: Number,
+			required: true,
+		},
+
+		propertyRooms: {
+			type: Number,
+			required: true,
+		},
+
+		propertyViews: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyLikes: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyComments: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyRank: {
+			type: Number,
+			default: 0,
+		},
+
+		propertyImages: {
+			type: [String],
+			required: true,
+		},
+
+		propertyDesc: {
+			type: String,
+		},
+
+		propertyRent: {
+			type: Boolean,
+			default: false,
+		},
+
+		memberId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'Member',
+		},
+
+		soldAt: {
+			type: Date,
+		},
+
+		deletedAt: {
+			type: Date,
+		},
+
+		constructedAt: {
+			type: Date,
+		},
+	},
+	{ timestamps: true, collection: 'properties' },
+);
+
+PropertySchema.index({ propertyType: 1, propertyLocation: 1, propertyTitle: 1, propertyPrice: 1 }, { unique: true });
+
+export default PropertySchema;
