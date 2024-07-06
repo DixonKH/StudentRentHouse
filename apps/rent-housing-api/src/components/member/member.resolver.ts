18b4dede925b10691dbc3ab2d3708757b293a220
DixonKH <khaydarovdilshod9@gmail.com>
2024-07-07 01:41:45 +0900

    feat: develop likeTargetMember GraphQL API

--- apps/rent-housing-api/src/components/member/member.resolver.ts
+++ apps/rent-housing-api/src/components/member/member.resolver.ts
@@ -80,6 +80,14 @@ export class MemberResolver {
 		return await this.memberService.getAgents(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTargetMember(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Member> {
+		console.log('Mutation: likeTargetMember');
+		const likeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.likeTargetMember(memberId, likeRefId);
+	}
+
 	/** ADMIN  */
 
 	@Roles(MemberType.ADMIN)
