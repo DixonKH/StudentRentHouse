32bc63a94e7db9aebbd8766117ff4af0dd1300af
DixonKH <khaydarovdilshod9@gmail.com>
2024-07-07 16:09:19 +0900

    feat: develop job schedular business logic

--- apps/rent-housing-batch/src/rent-housing-batch.controller.ts
+++ apps/rent-housing-batch/src/rent-housing-batch.controller.ts
@@ -1,12 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { RentHousingBatchService } from './rent-housing-batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class RentHousingBatchController {
-  constructor(private readonly rentHousingBatchService: RentHousingBatchService) {}
+	private logger: Logger = new Logger('BatchController');
+	constructor(private readonly rentHousingBatchService: RentHousingBatchService) {}
 
-  @Get()
-  getHello(): string {
-    return this.rentHousingBatchService.getHello();
-  }
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY!');
+	}
+
+	@Cron('00 00 01 * * *', { name: BATCH_ROLLBACK })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = BATCH_ROLLBACK;
+			this.logger.debug('EXECUTED');
+			await this.rentHousingBatchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20 00 01 * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchTopProperties() {
+		try {
+			this.logger['context'] = BATCH_TOP_PROPERTIES;
+			this.logger.debug('EXECUTED');
+			await this.rentHousingBatchService.batchTopProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('40 00 01 * * *', { name: BATCH_TOP_AGENTS })
+	public async batchTopAgents() {
+		try {
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTED');
+			await this.rentHousingBatchService.batchTopAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	/* 
+	@Interval(1000)
+	handleInterval() {
+		this.logger.debug(`INTERNAL TEST`);
+	}
+	*/
+
+	@Get()
+	getHello(): string {
+		return this.rentHousingBatchService.getHello();
+	}
 }
