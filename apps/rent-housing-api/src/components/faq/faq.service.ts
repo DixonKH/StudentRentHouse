1cb7bdbc223d6679ceacc9611fbf279a63fd9d1a
DixonKH <khaydarovdilshod9@gmail.com>
2024-07-28 06:11:13 +0900

    feat: develop faq module

--- apps/rent-housing-api/src/components/faq/faq.service.ts
+++ apps/rent-housing-api/src/components/faq/faq.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Faq } from '../../libs/dto/faq/faq';
+import { MemberService } from '../member/member.service';
+import { ViewService } from '../view/view.service';
 
 @Injectable()
-export class FaqService {}
+export class FaqService {
+	constructor(
+		@InjectModel('Faq') private readonly faqModel: Model<Faq>,
+		private readonly memberService: MemberService,
+		private viewService: ViewService,
+	) {}
+}
